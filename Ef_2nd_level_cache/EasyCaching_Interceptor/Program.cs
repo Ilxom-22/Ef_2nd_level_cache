6516fd8d16db00305d3b58d09743942df0070f33
Ilhom Karimjonov <ilxomkarimjonov22@gmail.com>
2024-02-15 12:06:43 +0500

    Configure the demo project

--- Ef_2nd_level_cache/EasyCaching_Interceptor/Program.cs
+++ Ef_2nd_level_cache/EasyCaching_Interceptor/Program.cs
@@ -1,6 +1,26 @@
+using EasyCaching_Interceptor.Persistence;
+using EasyCaching_Interceptor.SeedData;
+using EasyCaching_Interceptor.Services;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseInMemoryDatabase("2ndLevelCache"));
+
+builder.Services.AddScoped<UserService>();
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+await app.Services.CreateScope().ServiceProvider.SeedUsersAsync();
 
-app.Run();
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.MapControllers();
+
+await app.RunAsync();
